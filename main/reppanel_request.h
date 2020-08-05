044192214340641c3da6aabee7acad966b3cac79
Denis Voskvitsov <denis.voskvitsov@gmail.com>
2020-08-05 20:38:55 +0300

    chore(request): decrease JSON buffer size

--- main/reppanel_request.h
+++ main/reppanel_request.h
@@ -6,7 +6,7 @@
 #define REPPANEL_ESP32_REPPANEL_REQUEST_H
 
 #define MAX_REQ_ADDR_LENGTH     256 + 512
-#define JSON_BUFF_SIZE          1024 * 6        // d2wc settings is > 2800 bytes
+#define JSON_BUFF_SIZE          1024 * 4        // d2wc settings is > 2800 bytes
 #define UART_RESP_BUFF_SIZE     1024 * 4
 
 typedef struct {
